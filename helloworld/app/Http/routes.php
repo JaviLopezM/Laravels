a0b6cc178c0de8107e45a55421b50f36751f3110
Javi <javierlopez1@iesebre.com>
2015-10-19 17:57:50 +0200

    proves de status al routes

--- helloworld/app/Http/routes.php
+++ helloworld/app/Http/routes.php
@@ -1,24 +1,65 @@
 <?php
-use App\HolaMon;
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
+use App\HolaMon;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/helloworld',
+    "HolaMon@hello");
 
-//Route::get('/helloworld' ,
-//    "HolaMon@hello");
+Route::get('/helloworld2',"HolaMon@hello2");
+
+Route::resource('user', 'UserController');
+
+Route::get('/prova1', function () {
+    return '<h1>Hello World</h1>';
+});
+
+Route::post('/prova1', function () {
+    return '<h1>Hello World</h1>';
+});
+
+Route::get('/prova2', function () {
+
+$content = "<h1>Hello World</h1>" ;
+$status =200;
+$contentType ="aplication/pdf" ;
+return response($content , $status)
+->header ("Content-Type" , $contentType);
+});
+
+Route::get('/prova3', function () {
+
+    $content = "<h1>Hello World</h1>" ;
+    $status =200;
+    $contentType ="aplication/pdf" ;
+    return response($content , $status)
+        ->header ("Content-Type" , $contentType);
+});
+
+Route::get('/prova4', function () {
+    $pathToFile = public_path('prova.pdf');
+    dd ($pathToFile);
+});
+
+Route::get('/prova5', function () {
+    $pathToFile = public_path('prova.pdf');
+    echo $pathToFile;
+
+});
+
+Route::get('/prova6', function () {
+
+    return [
+        "name" => 'Abigail' , 'state' => 'CA'];
+
+});
 
 
-Route::get('/helloworld', "HolaMon@hello");
 
-Route::get('/helloworld2', "HolaMon@hello2");
+Route::get('/prova7', function () {
+
+    return redirect("http://www.google.com");
+
+});
+
