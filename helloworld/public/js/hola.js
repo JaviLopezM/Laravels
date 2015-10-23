0ebda88404f3f1100b4bacfef6f7b139257b195e
Javi <javierlopez1@iesebre.com>
2015-10-23 16:22:22 +0200

    JQuery en funció anònima.

--- helloworld/public/js/hola.js
+++ helloworld/public/js/hola.js
@@ -5,3 +5,12 @@ console.info("hola mon!");
 function bye(){
     console.info("bye mon!");
 }
+
+$(function(){
+
+    $("#sayHello").click(function(){
+        // console.debug("Prova");
+        // alert("Hola!!");
+        $("#helloworld").show();
+    });
+});
